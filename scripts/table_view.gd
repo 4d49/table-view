679e12f7cb4020fd42ea0861bd0a35e2c0d8b4ff
Mansur Isaev <737dab2f169a@mail.ru>
2024-09-25 10:20:59 +0400

    Add flags type support

--- scripts/table_view.gd
+++ scripts/table_view.gd
@@ -38,6 +38,7 @@ enum Hint {
 	NONE = PROPERTY_HINT_NONE,
 	RANGE = PROPERTY_HINT_RANGE,
 	ENUM = PROPERTY_HINT_ENUM,
+	FLAGS = PROPERTY_HINT_FLAGS,
 	COLOR_NO_ALPHA = PROPERTY_HINT_COLOR_NO_ALPHA,
 }
 enum DrawMode {
@@ -615,6 +616,18 @@ static func stringifier_default(type: Type, hint: Hint, hint_string: String) -> 
 			return func(value: int) -> String:
 				return values.get(value, "")
 
+		Type.INT when hint == Hint.FLAGS:
+			var flags := hint_string_to_flags(hint_string)
+
+			return func(value: int) -> String:
+				var string: String = ""
+
+				for key: String in flags:
+					if value & flags[key]:
+						string += key + ", "
+
+				return string.left(-2)
+
 		Type.FLOAT:
 			return hint_string.num.bind(NUMBERS_AFTER_DOT)
 		Type.COLOR:
@@ -672,7 +685,26 @@ static func hint_string_to_enum(hint_string: String) -> Dictionary[StringName, i
 		else:
 			enumeration[StringName(subsplit[0])] = i
 
+	enumeration.make_read_only()
 	return enumeration
+
+
+static func flags_to_hint_string(flags: Dictionary) -> String:
+	return enum_to_hint_string(flags)
+
+static func hint_string_to_flags(hint_string: String) -> Dictionary[StringName, int]:
+	var flags: Dictionary[StringName, int] = {}
+
+	var split: PackedStringArray = hint_string.split(",")
+	for i: int in split.size():
+		var subsplit: PackedStringArray = split[i].split(":")
+		if subsplit.size() > 1:
+			flags[StringName(subsplit[0])] = subsplit[1].to_int()
+		else:
+			flags[StringName(subsplit[0])] = 1 << i
+
+	flags.make_read_only()
+	return flags
 
 
 func set_cell_editor(cell_editor: Node) -> void:
@@ -693,7 +725,6 @@ func edit_handler_default(type: Type, hint: Hint, hint_string: String) -> Callab
 
 		Type.INT when hint == Hint.ENUM:
 			var enumeration := hint_string_to_enum(hint_string)
-			enumeration.make_read_only()
 
 			return func(cell: Dictionary, setter: Callable, getter: Callable) -> void:
 				var popup := PopupMenu.new()
@@ -708,6 +739,46 @@ func edit_handler_default(type: Type, hint: Hint, hint_string: String) -> Callab
 					popup.add_item(key, enumeration[key])
 
 				popup.id_pressed.connect(setter)
+				popup.focus_exited.connect(popup.queue_free)
+				self.add_child(popup)
+
+				var rect := scrolled_rect(cell.rect)
+				popup.set_position(rect.position)
+				popup.set_size(rect.size)
+
+				popup.set_meta(&"cell", cell)
+				self.set_cell_editor(popup)
+
+				popup.popup()
+
+		Type.INT when hint == Hint.FLAGS:
+			var flags := hint_string_to_flags(hint_string)
+
+			return func(cell: Dictionary, setter: Callable, getter: Callable) -> void:
+				var popup := PopupMenu.new()
+				popup.add_theme_font_override(&"font", _font)
+				popup.add_theme_font_size_override(&"font_size", _font_size)
+				popup.add_theme_color_override(&"font_color", _font_color)
+				popup.add_theme_constant_override(&"outline_size", _font_outline_size)
+				popup.add_theme_color_override(&"font_outline_color", _font_outline_color)
+				popup.add_theme_stylebox_override(&"panel", _cell_edit)
+
+				var value: int = getter.call()
+				for key: StringName in flags:
+					popup.add_check_item(key, flags[key])
+					popup.set_item_checked(-1, value & flags[key])
+
+				popup.id_pressed.connect(func(id: int) -> void:
+					value = getter.call()
+
+					if popup.is_item_checked(popup.get_item_index(id)):
+						value &= ~id
+					else:
+						value |= id
+
+					popup.set_item_checked(popup.get_item_index(id), not popup.is_item_checked(popup.get_item_index(id)))
+					setter.call(value)
+				)
 				popup.focus_exited.connect(popup.queue_free)
 				self.add_child(popup)
 
