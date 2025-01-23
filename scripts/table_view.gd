411d9379c76316a5d1f583f8167654940c94aea0
Mansur Isaev <737dab2f169a@mail.ru>
2025-01-23 11:37:33 +0400

    Add hover draw mode for row

--- scripts/table_view.gd
+++ scripts/table_view.gd
@@ -133,6 +133,7 @@ var _font_outline_color: Color = Color.BLACK
 var _panel: StyleBox = null
 var _focus: StyleBox = null
 
+var _row_hover: StyleBox = null
 var _row_normal: StyleBox = null
 var _row_selected: StyleBox = null
 var _row_alternate: StyleBox = null
@@ -306,6 +307,7 @@ func _notification(what: int) -> void:
 			RenderingServer.canvas_item_set_custom_rect(_canvas, true, drawable_rect)
 			RenderingServer.canvas_item_set_clip(_canvas, true)
 
+			var mouse_position: Vector2 = get_local_mouse_position()
 			var draw_begun: bool = false
 
 			var idx: int = 0
@@ -323,6 +325,8 @@ func _notification(what: int) -> void:
 
 				if row.selected:
 					_row_selected.draw(_canvas, rect)
+				elif rect.has_point(mouse_position):
+					_row_hover.draw(_canvas, rect)
 				elif idx % 2:
 					_row_alternate.draw(_canvas, rect)
 				else:
@@ -390,6 +394,7 @@ func _notification(what: int) -> void:
 			_panel = get_theme_stylebox(&"panel", &"TableView")
 			_focus = get_theme_stylebox(&"focus", &"TableView")
 
+			_row_hover = get_theme_stylebox(&"row_hover", &"TableView")
 			_row_normal = get_theme_stylebox(&"row_normal", &"TableView")
 			_row_selected = get_theme_stylebox(&"row_selected", &"TableView")
 			_row_alternate = get_theme_stylebox(&"row_alternate", &"TableView")
